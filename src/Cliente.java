e36f79ca0f381ae46c0cf8bea0210ef8a4c17490
Erinaldo <erinaldo2005@gmail.com>
2024-06-06 23:29:11 -0300

    Trabalhando a abstração

---
+++ src/Cliente.java
@@ -0,0 +1,10 @@
+public class Cliente {
+    String nome;
+    String cpf;
+
+    public void sacar(){ };
+
+    public void depositar() {};
+
+    public void transferir() {};
+}
---
+++ src/ContaCorrente.java
@@ -0,0 +1,6 @@
+public class ContaCorrente {
+    public int agencia;
+    public int numeroConta;
+    public double saldo;
+
+}
